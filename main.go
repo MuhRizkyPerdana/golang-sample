a0d07a5d74140fcec5f406c4c3ff63937e0a9706
adhithia <37793374+aditirvan@users.noreply.github.com>
2022-12-01 19:30:37 +0700

    goapp 8000

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
+	"os"
 )
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage 8000!\nApp Version: 3.0")
+}
+
+func envPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List of environment variable:")
+	for _, env := range os.Environ() {
+		fmt.Fprintf(w, "- "+env+"\n")
+	}
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/env", envPage)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
 func main() {
-	currentTime := time.Now()
-	fmt.Printf("Waktu sekarang adalah: %d-%d-%d %d:%d:%d\n",
-		currentTime.Year(),
-		currentTime.Month(),
-		currentTime.Day(),
-		currentTime.Hour(),
-		currentTime.Hour(),
-		currentTime.Second())
+	handleRequests()
 }
