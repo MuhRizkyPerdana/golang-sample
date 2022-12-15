642b121f5e9c3047dced8744ff9a800dcba88291
adhithia21 <116649263+adhithia21@users.noreply.github.com>
2022-12-15 22:20:05 +0700

    update to version 5

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Golang Sample, Selamat Datang!\nApp Version: 4")
+	fmt.Fprintf(w, "Golang Sample, Selamat Datang!\nApp Version: 5")
 }
 
 func envPage(w http.ResponseWriter, r *http.Request) {
